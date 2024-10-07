1ed8e6819cf5f7a707d7e901abf75bc49b3462b4
Ahasan Ullah <ahasanullah451@gmail.com>
2024-10-07 23:53:00 +0600

    post functionality added

--- script/script.js
+++ script/script.js
@@ -1,10 +1,59 @@
 const loadAllPosts=async(searchText)=>{
-  const res=await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`);
+  const res=await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts${searchText?`?category=${searchText}`:''}`);
   const data=await res.json();
-  console.log(data.posts);
+  displayAllPosts(data.posts);
 };
 
-
+const displayAllPosts=(posts)=>{
+  const postContainer=document.getElementById('post-container');
+  posts.forEach(post=>{
+    const div=document.createElement('div');
+    div.innerHTML=
+    `
+      <div class="p-6 flex gap-6 lg:flex-row flex-col items-center lg:items-start bg-[#F3F3F5] rounded-3xl">
+        <div class="indicator">
+          <span class="indicator-item badge ${post.isActive?"bg-green-600":"bg-red-500"}"></span>
+          <div class="avatar">
+            <div class="w-24 rounded-xl">
+              <img src=${post.image}/>
+            </div>
+          </div>
+        </div>
+        <div class="space-y-4 w-full">
+          <div class="flex gap-4 *:opacity-60">
+            <p># ${post.category}</p>
+            <p>Author: ${post.author.name}</p>
+          </div>
+        <h3 class="text-2xl font-bold opacity-70">${post.title}</h3>
+        <p class="opacity-40">${post.description}</p>
+        <hr class="border border-dashed border-gray-300"/>
+        <div class="flex justify-between *:font-bold [&>*:not(:last-child)]:opacity-45">
+          <div class="gap-4">
+            <div class="space-x-2 flex items-center">
+              <i class="fa-regular fa-comment-dots"></i>
+              <p>${post.comment_count}</p>
+            </div>
+            <div class="space-x-2 flex items-center">
+              <i class="fa-regular fa-eye"></i>
+              <p>${post.view_count}</p>
+            </div>
+            <div class="space-x-2 flex items-center">
+              <i class="fa-regular fa-clock"></i>
+              <p>${post.posted_time} Min</p>
+            </div>
+          </div>
+          <div class="opacity-100">
+            <button id="addToList" onclick="markAsRead()"
+              data-post='${JSON.stringify(post)}' class="addToList btn btn-circle bg-green-500 btn-sm"><i class="fa-solid fa-envelope-open text-white"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+    postContainer.appendChild(div);
+  });
+}
 
 loadAllPosts();
 const handleSearchByCategory=()=>{
